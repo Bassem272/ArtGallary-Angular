7a8b360eb87f5f4b0afbbddd3fd23a23f467eb76
Bassem272 <73647090+Bassem272@users.noreply.github.com>
2023-09-10 01:59:06 +0300

    easd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthenticationModule } from './authentication/authentication.module';
-
 
 // Angular Material modules
 import { MatButtonModule } from '@angular/material/button';
@@ -16,32 +13,30 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon'; // If you want to use icons in the card
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { FooterComponent } from './shared/footer/footer.component';
-import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faFilm } from '@fortawesome/free-solid-svg-icons';
-@NgModule({
-  declarations: [
-    AppComponent,FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule, AuthenticationModule ,
-MatCardModule,MatIconModule,
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
 
+@NgModule({
+  declarations: [AppComponent, FooterComponent],
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AuthenticationModule,
+    MatCardModule,
+    MatIconModule,
     MatButtonModule,
     MatInputModule,
-    MatToolbarModule,MatFormFieldModule,MatExpansionModule
-  ,FontAwesomeModule
-
-
-],
+    MatToolbarModule,
+    MatFormFieldModule,
+    BrowserModule,
+    MatExpansionModule,
+    FontAwesomeModule
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-  constructor(library: FaIconLibrary) {
-    library.addIcons(faFilm);
-  }
-}
+export class AppModule {}
